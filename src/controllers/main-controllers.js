87a9aa51cdfe50ff5a19e67571dd7096470541a3
Leonel <leonelbenjaminmercau@gmail.com>
2023-12-06 12:18:17 -0300

    rutas

--- src/controllers/main-controllers.js
+++ src/controllers/main-controllers.js
@@ -4,7 +4,24 @@ const path = require('path')
 const controller = {
     home: (req, res) => {
         res.sendFile(path.join(__dirname, '../views/home.html'));
-    }
+    },
+
+    catalogo: (req, res) => {
+        res.sendFile(path.join(__dirname, '../views/catalogo.html'));
+    },
+
+    contacto: (req, res) => {
+        res.sendFile(path.join(__dirname, '../views/contacto.html'));
+    },
+
+    detalleProd: (req, res) => {
+        res.sendFile(path.join(__dirname, '../views/detalleProd.html'));
+    },
+
+    carrito: (req, res) => {
+        res.sendFile(path.join(__dirname, '../views/carrito.html'));
+    },
+
 }
 
 module.exports = controller;
--- src/routes/main-routes.js
+++ src/routes/main-routes.js
@@ -4,5 +4,9 @@ const router = express.Router();
 const mainController = require("../controllers/main-controllers");
 
 router.get('/', mainController.home);
+router.get('/catalogo', mainController.catalogo);
+router.get('/contacto', mainController.contacto);
+router.get('/detalleProd', mainController.detalleProd);
+router.get('/carrito', mainController.carrito );
 
 module.exports = router;
