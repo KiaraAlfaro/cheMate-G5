c674fa8b3327a1df442228246a965d72f772e5e0
kiarAlfaro <arcealfarokiara@gmail.com>
2023-12-06 00:15:29 -0300

    agregado de carpetas scr, public y archicos js

---
+++ src/controllers/main-controllers.js
@@ -0,0 +1,10 @@
+
+const path = require('path')
+
+const controller = {
+    home: (req, res) => {
+        res.sendFile(path.join(__dirname, '../views/home.html'));
+    }
+}
+
+module.exports = controller;
