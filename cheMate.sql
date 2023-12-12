424a101e5e690eb31a8862b06d0c9e905cf32cde
ImanolRV <imanol-rv18@hotmail.com>
2023-12-12 02:14:57 -0300

    Update cheMate.sql

--- cheMate.sql
+++ cheMate.sql
@@ -3,15 +3,15 @@ CREATE DATABASE cheMate_db;
 
 USE cheMate_db;
 
-CREATE TABLE categories (
+CREATE TABLE category (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
 );
 
-INSERT INTO categories (name) VALUES ('Mates'),('Termos'),('Bombillas'),('Materos'),
+INSERT INTO category (name) VALUES ('Mates'),('Termos'),('Bombillas'),('Materos'),
 
 
-CREATE TABLE products (
+CREATE TABLE product (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     --category_id INT NOT NULL,
@@ -19,11 +19,21 @@ CREATE TABLE products (
     description VARCHAR(255) NOT NULL,
     img VARCHAR(255) NOT NULL
 );
-INSERT INTO products (name, price, description, img) VALUES ('Pan 1', 1, 100, 'Descripción 1'),('Pan 2', 1, 200, 'Descripción 2'),('Pan 3', 1, 200, 'Descripción 3'),('Pan 4', 1, 200, 'Descripción 4'),('Pan 5', 2, 200, 'Descripción 5');
+INSERT INTO product (name, price, description, img) 
+VALUES 
+    ('Mate 1', 1000, 'Descripcion 1', 'img/mates/01.jpg'),
+    ('Mate 2', 2000, 'Descripcion 2', 'img/mates/02.jpg'),
+    ('Mate 3', 3000, 'Descripcion 3', 'img/mates/03.jpg'),
+    ('Mate 4', 4000, 'Descripcion 4', 'img/mates/04.jpg'),
+    ('Mate 5', 5000, 'Descripcion 5', 'img/mates/05.jpg'),
+    ('Mate 6', 6000, 'Descripcion 6', 'img/mates/06.jpg'),
+    ('Mate 7', 7000, 'Descripcion 7', 'img/mates/07.jpg'),
+    ('Mate 8', 8000, 'Descripcion 8', 'img/mates/08.jpg');
 
 
-CREATE TABLE category_products (
+
+CREATE TABLE category_product (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    products_id INT NOT NULL,
-    categories_id INT NOT NULL
+    product_id INT NOT NULL,
+    category_id INT NOT NULL
 );
